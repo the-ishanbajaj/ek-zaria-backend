7f44e1f1fa7e339ad143380c98799101dc4695dc
Ishan Bajaj <121756513+the-ishanbajaj@users.noreply.github.com>
2025-11-03 16:53:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const multer = require('multer')
+const fs = require('fs')        
+const path = require('path')
 
 const app = express()
 
